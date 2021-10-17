b39e559f0e0882bf538218cb65f189f86282ac7a
Bernhard Haumacher <haui@haumacher.de>
2021-10-17 14:23:03 +0200

    Streamlined ETS parsing.

--- src/main/java/com/tellerulam/knx2mqtt/EtsLoader.java
+++ src/main/java/com/tellerulam/knx2mqtt/EtsLoader.java
@@ -37,11 +37,11 @@ public class EtsLoader {
 
 	private final GroupAddressManager _addressManager;
 
-	private Map<String, Map<String, Map<String, String>>> deviceDescriptionCache;
+	private Map<String, Map<String, Map<String, String>>> _deviceDescriptionCache;
 
-	private Map<Integer, String> dptMap;
+	private Map<Integer, String> _dptMap;
 
-	private SAXParserFactory saxFactory;
+	private SAXParserFactory _saxFactory;
 
 	/**
 	 * Creates a {@link EtsLoader}.
@@ -92,19 +92,20 @@ public class EtsLoader {
 	 */
 	private void loadProjectFile(File projectFile) {
 		long startTime = System.currentTimeMillis();
-		try (ZipFile zf = new ZipFile(projectFile)) {
+		try (ZipFile zip = new ZipFile(projectFile)) {
 			// Find the project file
-			Enumeration<? extends ZipEntry> entries = zf.entries();
+			Enumeration<? extends ZipEntry> entries = zip.entries();
 			while (entries.hasMoreElements()) {
 				ZipEntry ze = entries.nextElement();
 				if (ze.getName().toLowerCase().endsWith("project.xml")) {
 					String projDir = ze.getName().substring(0, ze.getName().indexOf('/') + 1);
 					L.info("Found project directory " + projDir);
 					// Now find the project data file
-					ZipEntry zep = zf.getEntry(projDir + "0.xml");
-					if (zep == null)
+					ZipEntry projectEntry = zip.getEntry(projDir + "0.xml");
+					if (projectEntry == null) {
 						throw new IllegalArgumentException("Unable to locate 0.xml in project");
-					processETS4ProjectFile(zf, zep);
+					}
+					loadProjectFile(zip, projectEntry);
 					break;
 				}
 			}
@@ -119,12 +120,12 @@ public class EtsLoader {
 	/**
 	 * First step in parsing: find the GroupAddresses and their IDs
 	 */
-	private void processETS4ProjectFile(ZipFile zf, ZipEntry zep)
+	private void loadProjectFile(ZipFile zip, ZipEntry projectEntry)
 			throws ParserConfigurationException, SAXException, IOException, KNXException {
 		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 		docBuilderFactory.setCoalescing(true);
 		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-		Document doc = docBuilder.parse(zf.getInputStream(zep));
+		Document doc = docBuilder.parse(zip.getInputStream(projectEntry));
 		NodeList gas = doc.getElementsByTagName("GroupAddress");
 		NodeList sendConnections = doc.getElementsByTagName("Send");
 		NodeList receiveConnections = doc.getElementsByTagName("Receive");
@@ -154,8 +155,8 @@ public class EtsLoader {
 			}
 
 			// We're not lucky. Look into the connections
-			processETS4GroupAddressConnections(zf, doc, sendConnections, receiveConnections, e.getAttribute("Id"),
-					address, name);
+			processETS4GroupAddressConnections(zip, sendConnections, receiveConnections, e.getAttribute("Id"), address,
+					name);
 		}
 	}
 
@@ -191,8 +192,8 @@ public class EtsLoader {
 	/**
 	 * Find out what is connected to this group address
 	 */
-	private void processETS4GroupAddressConnections(ZipFile zf, Document doc, NodeList sendConnections,
-			NodeList receiveConnections, String id, String address, String name)
+	private void processETS4GroupAddressConnections(ZipFile zip, NodeList sendConnections, NodeList receiveConnections,
+			String id, String address, String name)
 			throws SAXException, IOException, ParserConfigurationException, KNXException {
 		boolean foundConnection = false;
 		for (int attempt = 0; attempt < 4; attempt++) {
@@ -221,7 +222,7 @@ public class EtsLoader {
 						return;
 					}
 					/* No luck, no luck. Dig deeper */
-					if (processETS4GroupConnection(zf, pe.getAttribute("RefId"), id, address, name, useObjectSize))
+					if (processETS4GroupConnection(zip, pe.getAttribute("RefId"), address, name, useObjectSize))
 						return;
 				}
 			}
@@ -234,18 +235,17 @@ public class EtsLoader {
 					"Unable to determine datapoint type for " + id + "/" + address + "/" + name);
 	}
 
-	private boolean processETS4GroupConnection(ZipFile zf, String refId, String id, String address, String name,
-			boolean useObjectSize) throws SAXException, IOException, ParserConfigurationException, KNXException {
+	private boolean processETS4GroupConnection(ZipFile zip, String refId, String address, String name, boolean useObjectSize) throws SAXException, IOException, ParserConfigurationException, KNXException {
 		// Right, we need to look into the device description. Determine it's
 		// filename
 		String refIdParts[] = refId.split("_");
 		String pathName = refIdParts[0] + "/" + refIdParts[0] + "_" + refIdParts[1] + ".xml";
-		Map<String, Map<String, String>> dev = loadDeviceDescription(zf, pathName);
+		Map<String, Map<String, String>> dev = loadDeviceDescription(zip, pathName);
 		Map<String, String> cobjref = dev.get(refId);
 		if (cobjref == null)
 			throw new IllegalArgumentException("Unable to find ComObjectRef with Id " + refId + " in " + pathName);
 		// Perhaps the ComObjectRef
-		if (processETS4ComObj(cobjref, zf, address, name, useObjectSize))
+		if (processETS4ComObj(cobjref, zip, address, name, useObjectSize))
 			return true;
 
 		String refco = cobjref.get("RefId");
@@ -253,13 +253,13 @@ public class EtsLoader {
 		if (cobj == null)
 			throw new IllegalArgumentException("Unable to find ComObject with Id " + refco + " in " + pathName);
 
-		if (processETS4ComObj(cobj, zf, address, name, useObjectSize))
+		if (processETS4ComObj(cobj, zip, address, name, useObjectSize))
 			return true;
 
 		return false;
 	}
 
-	private boolean processETS4ComObj(Map<String, String> cobj, ZipFile zf, String address, String name,
+	private boolean processETS4ComObj(Map<String, String> cobj, ZipFile zip, String address, String name,
 			boolean useObjectSize) throws SAXException, IOException, ParserConfigurationException, KNXException {
 		String dpt = cobj.get("DatapointType");
 		if (dpt != null && dpt.length() != 0) {
@@ -274,14 +274,14 @@ public class EtsLoader {
 					L.warning("Warning: Infering DPT for " + new GroupAddress(Integer.parseInt(address)) + " (" + name
 							+ ") by objSize " + objSize
 							+ " - this is not good, please update your ETS4/ETS project with proper DPT specifications!");
-				storeGAInfo(address, name, inferDPTFromObjectSize(zf, objSize));
+				storeGAInfo(address, name, inferDPTFromObjectSize(zip, objSize));
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private String inferDPTFromObjectSize(ZipFile zf, String objSize)
+	private String inferDPTFromObjectSize(ZipFile zip, String objSize)
 			throws SAXException, IOException, ParserConfigurationException {
 		// Take a guess based on size
 		String dpitid = null;
@@ -293,52 +293,55 @@ public class EtsLoader {
 		else if ("2 Bytes".equals(objSize))
 			dpitid = "9-1";
 		else {
-			if (dptMap == null) {
-				dptMap = new HashMap<>();
-				SAXParser saxParser = saxFactory.newSAXParser();
-				DefaultHandler gaHandler = new DefaultHandler() {
-					private String currentSize;
-
-					@Override
-					public void startElement(String uri, String localName, String qName, Attributes attr)
-							throws SAXException {
-						if ("DatapointType".equals(qName)) {
-							currentSize = attr.getValue("SizeInBit");
-						} else if ("DatapointSubtype".equals(qName)) {
-							if (currentSize != null) {
-								dptMap.put(Integer.valueOf(currentSize), attr.getValue("Id"));
-								currentSize = null;
-							}
-						}
-					}
-				};
-				saxParser.parse(zf.getInputStream(new ZipEntry("knx_master.xml")), gaHandler);
+			if (_dptMap == null) {
+				_dptMap = loadDptMap(zip);
 			}
 			String sizeSpec[] = objSize.split(" ");
 			int bits = Integer.parseInt(sizeSpec[0]);
 			if (sizeSpec[1].startsWith("Byte"))
 				bits *= 8;
-			return dptMap.get(bits);
+			return _dptMap.get(bits);
 		}
 		return "DPST-" + dpitid;
 	}
 
-	private Map<String, Map<String, String>> loadDeviceDescription(ZipFile zf, String filename)
+	private Map<Integer, String> loadDptMap(ZipFile zip)
 			throws ParserConfigurationException, SAXException, IOException {
-		if (deviceDescriptionCache == null) {
-			saxFactory = SAXParserFactory.newInstance();
-			deviceDescriptionCache = new HashMap<>();
+		Map<Integer, String> dptMap = new HashMap<>();
+		DefaultHandler gaHandler = new DefaultHandler() {
+			private String currentSize;
+
+			@Override
+			public void startElement(String uri, String localName, String qName, Attributes attr)
+					throws SAXException {
+				if ("DatapointType".equals(qName)) {
+					currentSize = attr.getValue("SizeInBit");
+				} else if ("DatapointSubtype".equals(qName)) {
+					if (currentSize != null) {
+						dptMap.put(Integer.valueOf(currentSize), attr.getValue("Id"));
+						currentSize = null;
+					}
+				}
+			}
+		};
+		newSaxParser().parse(zip.getInputStream(new ZipEntry("knx_master.xml")), gaHandler);
+		return dptMap;
+	}
+
+	private Map<String, Map<String, String>> loadDeviceDescription(ZipFile zip, String filename)
+			throws ParserConfigurationException, SAXException, IOException {
+		if (_deviceDescriptionCache == null) {
+			_deviceDescriptionCache = new HashMap<>();
 		} else {
-			Map<String, Map<String, String>> cacheEntry = deviceDescriptionCache.get(filename);
+			Map<String, Map<String, String>> cacheEntry = _deviceDescriptionCache.get(filename);
 			if (cacheEntry != null) {
 				return cacheEntry;
 			}
 		}
-		ZipEntry ze = zf.getEntry(filename);
+		ZipEntry ze = zip.getEntry(filename);
 		if (ze == null)
 			throw new IllegalArgumentException("Unable to find device description " + filename);
 		final Map<String, Map<String, String>> attrById = new HashMap<>();
-		SAXParser saxParser = saxFactory.newSAXParser();
 		DefaultHandler gaHandler = new DefaultHandler() {
 			@Override
 			public void startElement(String uri, String localName, String qName, Attributes attr) throws SAXException {
@@ -351,9 +354,16 @@ public class EtsLoader {
 				}
 			}
 		};
-		saxParser.parse(zf.getInputStream(ze), gaHandler);
-		deviceDescriptionCache.put(filename, attrById);
+		newSaxParser().parse(zip.getInputStream(ze), gaHandler);
+		_deviceDescriptionCache.put(filename, attrById);
 		return attrById;
+	}
+
+	private SAXParser newSaxParser() throws ParserConfigurationException, SAXException {
+		if (_saxFactory == null) {
+			_saxFactory = SAXParserFactory.newInstance();
+		}
+		return _saxFactory.newSAXParser();
 	}
 
 }
